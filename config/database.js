32a0a750dd5b0d73698352b03011941cf7f94974
Ricardo Vieira da Silva <ricardo.web@ultrafarma.com.br>
2019-03-08 15:11:46 -0300

    ajuste na url do mongodb remoto

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV === 'production')
 {
     module.exports = {mongoURI : 
-    'mongodb://ricvsweb-nodejs1:shRAbTFxgfGXoLWm@firstnodejscrud-1usf8.mongodb.net/test?retryWrites=true'
+    'mongodb+srv://ricvsweb-nodejs1:shRAbTFxgfGXoLWm@firstnodejscrud-1usf8.mongodb.net/test?retryWrites=true'
     }
 }
 else
