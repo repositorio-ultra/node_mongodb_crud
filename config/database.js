5e53266a3755034d073cd1f3592837bdbae21d7c
ricvsweb <ricvsweb@gmail.com>
2019-06-17 09:08:53 -0300

    Ajuste do apontamento ao banco de dados

--- config/database.js
+++ config/database.js
@@ -7,7 +7,8 @@ if(process.env.NODE_ENV === 'production')
 else
 {
     module.exports = {mongoURI : 
-        'mmongodb://localhost/vidjot-dev'
+        //'mmongodb://localhost/vidjot-dev'
+        'mongodb+srv://ricvsweb-nodejs1:shRAbTFxgfGXoLWm@firstnodejscrud-1usf8.mongodb.net/test?retryWrites=true'
         }
 
 }
